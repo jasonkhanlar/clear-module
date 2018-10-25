fff0df0ff777e9c7c935ef3b7719da3c10171e73
Jason Khanlar <jason.khanlar@gmail.com>
2018-10-25 02:49:51 -0400

    updates

--- test.js
+++ test.js
@@ -9,34 +9,6 @@ test('clearModule()', t => {
 	t.is(require(id)(), 1);
 });
 
-test('clearModule("fixture", {children: true})', t => {
-	const id = './fixture';
-	console.log('breq', require(id)());
-	console.log('breq', require(id)());
-	//t.is(require(id)(), 1);
-	//t.is(require(id)(), 2);
-	m(id);
-	console.log('breq', require(id)());
-	//t.is(require(id)(), 1);
-
-	const scriptsCachedBeforeRequire = Object.keys(require.cache).length; // 5
-	const path = require('path');
-	const id2 = 'resolve-from';
-	const resolveFrom = require(id2);
-	const id3 = 'caller-path';
-	const callerPath = require(id3);
-	const id2Path = resolveFrom(path.dirname(callerPath()), id2);
-	const id2Dir = path.dirname(id2Path);
-	const id3Path = resolveFrom(path.dirname(callerPath()), id3);
-	const id3Dir = path.dirname(id3Path);
-	console.log(id2Dir);
-	console.log(id3Dir);
-	const scriptsCachedAfterRequire = Object.keys(require.cache).length; // 176
-	console.log(scriptsCachedBeforeRequire);
-	console.log(scriptsCachedAfterRequire);
-	t.is(require(id)(), 13);
-});
-
 test('clearModule.all()', t => {
 	t.true(Object.keys(require.cache).length > 0);
 	m.all();
@@ -52,3 +24,26 @@ test('clearModule.match()', t => {
 	t.is(require(id)(), 2);
 	t.is(require(match)(), 1);
 });
+
+test('clearModule("fixture", {children: true})', t => {
+	// First empty all cache
+	m.all();
+
+	const scriptsCachedBeforeRequire = Object.keys(require.cache).length; // 5
+	const path = require('path');
+	const id1 = 'resolve-from';
+	const resolveFrom = require(id1);
+	const id2 = 'caller-path';
+	const callerPath = require(id2);
+	const id1Path = resolveFrom(path.dirname(callerPath()), id1);
+	const id1Dir = path.dirname(id1Path);
+	const id2Path = resolveFrom(path.dirname(callerPath()), id2);
+	const id2Dir = path.dirname(id2Path);
+	console.log(id1Dir);
+	console.log(id2Dir);
+	const scriptsCachedAfterRequire = Object.keys(require.cache).length; // 176
+	console.log(scriptsCachedBeforeRequire);
+	console.log(scriptsCachedAfterRequire);
+	t.is(require(id)(), 13);
+});
+
